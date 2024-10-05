e3d94efbada5736429f0f27c75a4a17fb1f3f03f
Hineill <hineill.cespedes@pucp.edu.pe>
2024-10-05 12:14:59 -0500

    Hola

--- app/src/main/java/com/example/iot_lab4_20213704/MainActivity.java
+++ app/src/main/java/com/example/iot_lab4_20213704/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.iot_lab4_20213704;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -31,20 +34,40 @@ public class MainActivity extends AppCompatActivity {
 
     public void ingresar(View view){
         //Validar conexión a internet
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+        ConnectivityManager  conectividad = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        if (!isConnected) {
-            // Mostrar Dialog
-        }else{
-            Intent intent = new Intent(this, AppActivity.class);
-            startActivity(intent);
+        boolean tieneInternet = false;
+        if(conectividad !=null){
+            NetworkCapabilities capabilities = conectividad.getNetworkCapabilities(conectividad.getActiveNetwork());
+            if(capabilities != null){
+                Intent intent = new Intent(this,  AppActivity.class);
+                startActivity(intent);
+            }else{
+                //Se muestra dialog
+                showAlertDialog();
+            }
         }
+    }
 
+    private void showAlertDialog() {
+        // Crear un AlertDialog.Builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-        Intent intent = new Intent(Settings.ACTION_SETTINGS);
-        startActivity(intent);
+        // Establecer el título, mensaje y botones
+        builder.setTitle("No hay conexión a internet");
+        builder.setMessage("Conectate a internet");
 
+        // Botón "Aceptar"
+        builder.setPositiveButton("Configuración ", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_WIFI_SETTINGS);
+                startActivity(intent);
+            }
+        });
+
+        // Mostrar el AlertDialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
