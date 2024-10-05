002defb32c3368ef956fa8959777a4863d8b493a
Hineill <hineill.cespedes@pucp.edu.pe>
2024-10-05 13:33:04 -0500

    Holaa

--- app/src/main/java/com/example/iot_lab4_20213704/AppActivity.java
+++ app/src/main/java/com/example/iot_lab4_20213704/AppActivity.java
@@ -8,6 +8,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class AppActivity extends AppCompatActivity {
 
@@ -21,16 +24,40 @@ public class AppActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Seteamos le fragmento en la vista
+
+        Fragment miFragmento = new ListaFragment();  // Reemplaza con tu propio fragmento
+
+        // Obtener el FragmentManager y comenzar una transacción
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        // Reemplazar el contenedor con el nuevo fragmento
+        fragmentTransaction.replace(R.id.fragmento_total, miFragmento);
+        // Confirmar la transacción
+        fragmentTransaction.commit();
     }
 
     public void ligas(View view){
-
+        Fragment miFragmento = new ListaFragment();  // Reemplaza con tu propio fragmento
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmento_total, miFragmento);
+        fragmentTransaction.commit();
     }
 
     public void resultados(View view){
-
+        Fragment miFragmento = new ResultadosFragment();  // Reemplaza con tu propio fragmento
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmento_total, miFragmento);
+        fragmentTransaction.commit();
     }
     public void posiciones(View view){
-
+        Fragment miFragmento = new PosicionesFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmento_total, miFragmento);
+        fragmentTransaction.commit();
     }
 }
