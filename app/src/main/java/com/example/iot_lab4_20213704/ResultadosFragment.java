002f61cc3d332e31d44f55243a53d2a897350c05
Hineill <hineill.cespedes@pucp.edu.pe>
2024-10-05 20:32:54 -0500

    Acelerometro funcionando

--- app/src/main/java/com/example/iot_lab4_20213704/ResultadosFragment.java
+++ app/src/main/java/com/example/iot_lab4_20213704/ResultadosFragment.java
@@ -1,11 +1,18 @@
 package com.example.iot_lab4_20213704;
 
+import static android.content.Context.SENSOR_SERVICE;
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,21 +23,28 @@ import android.widget.Toast;
 import com.example.iot_lab4_20213704.Adapter.ListaPosicionesAdapter;
 import com.example.iot_lab4_20213704.Adapter.ListaResultadosAdapter;
 import com.example.iot_lab4_20213704.Beans.PositionBusqueda;
+import com.example.iot_lab4_20213704.Beans.Resultado;
 import com.example.iot_lab4_20213704.Beans.ResultadoBusqueda;
 import com.example.iot_lab4_20213704.Service.ServiceRetrofit;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import android.content.Context;
+import android.hardware.Sensor;
+
+import java.util.ArrayList;
+
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ResultadosFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ResultadosFragment extends Fragment {
+public class ResultadosFragment extends Fragment implements SensorEventListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -40,7 +54,6 @@ public class ResultadosFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
     public ResultadosFragment() {
         // Required empty public constructor
     }
@@ -62,7 +75,6 @@ public class ResultadosFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,11 +82,13 @@ public class ResultadosFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        mSensorManager = (SensorManager) requireActivity().getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
-
+    private SensorManager mSensorManager;
+    private Sensor accelerometer;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState){
         View view  = inflater.inflate(R.layout.fragment_resultados, container, false);
         Button b = view.findViewById(R.id.buscar);
         b.setOnClickListener(view1->{
@@ -84,13 +98,15 @@ public class ResultadosFragment extends Fragment {
 
             if(!idLiga.getText().toString().trim().isEmpty() && !season.getText().toString().trim().isEmpty() && !ronda.getText().toString().trim().isEmpty()){
                 buscarResultados(view,idLiga.getText().toString(),season.getText().toString(),ronda.getText().toString());
+                //Activamos el sensor
+                mSensorManager.registerListener((SensorEventListener) this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
             }else{
                 Toast.makeText(getContext(), "Debes llenar todos los campos", Toast.LENGTH_SHORT).show();
             }
         });
+        supreme = view;
         return view;
     }
-
 
     public void buscarResultados(View view , String idLiga,String temporada,String ronda){
 
@@ -127,4 +143,32 @@ public class ResultadosFragment extends Fragment {
         });
     }
 
+    View supreme = null;
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent){
+        if(Math.sqrt(Math.pow(sensorEvent.values[0] ,2)  +  Math.pow(sensorEvent.values[1] ,2)  + Math.pow(sensorEvent.values[2] ,2)) >20 ){
+            //Mostramos el dialog
+            new MaterialAlertDialogBuilder(getContext())
+                    .setTitle("Supero el umbral de 20m/s2")
+                    .setMessage("¿Eliminará sus resultados?")
+                    .setPositiveButton("Yes", (dialog, which) -> {
+                        //Eliminamos los datos
+                        ListaResultadosAdapter adapter = new ListaResultadosAdapter();
+                        adapter.setContext(getContext());
+                        adapter.setLista(new ArrayList<Resultado>());
+                        RecyclerView recyclerView = supreme.findViewById(R.id.recyclerPartidos);
+                        recyclerView.setAdapter(adapter);
+                        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+                    })
+                    .setNegativeButton("Nou", (dialog, which) -> {
+                        //No se hace nada
+                    })
+                    .show();
+        }
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+    }
 }
